6c97ebd46e17264b687eb8253f7f1778c1182973
raskem <maminskaite.rasa@gmail.com>
2023-03-13 11:18:42 +0200

    update conf to run on selenium-standalone-grid

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,41 +1,51 @@
 import ChromeService from "wdio-chromedriver-service";
 
 export const config = {
-    runner: 'local',
-    specs: [
-        './project/webTests/**/*.js'
-    ],
-    maxInstances: 10,
-    capabilities: [{
-        maxInstances: 5,
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-            args: ['headless', 'incognito']},
-        acceptInsecureCerts: true,
-        excludeDriverLogs: ['*']
-    }],
-    logLevel: 'debug',
-    bail: 0,
-    baseUrl: 'https://saucedemo.com',
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    services: ['chromedriver'],
-    framework: 'mocha',
-    reporters: [['allure', {
-        outputDir: 'allure-report',
+  runner: "local",
+  specs: ["./project/webTests/**/*.js"],
+  maxInstances: 10,
+  capabilities: [
+    {
+      maxInstances: 3,
+      browserName: "firefox",
+    },
+    {
+      browserName: "chrome",
+        "goog:chromeOptions": {
+      args: ["headless", "incognito"],
+      },
+      acceptInsecureCerts: true,
+      excludeDriverLogs: ["*"],
+    },
+  ],
+  logLevel: "debug",
+  bail: 0,
+  baseUrl: "https://saucedemo.com",
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  services: ["chromedriver", [ChromeService]],
+  framework: "mocha",
+  reporters: [
+    [
+      "allure",
+      {
+        outputDir: "allure-report",
         // disableWebdriverStepsReporting: true,
         //disableWebdriverScreenshotsReporting: false
-    }]],
-
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 60000
-    },
-   
-    afterTest: async function(test, context, { error, result, duration, passed, retries }) {
-        if (!passed) {
-            await browser.takeScreenshot();
-        }
+      },
+    ],
+  ],
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
+  afterTest: async function (test, context, { error, result, duration, passed, retries }) {
+    if (!passed) {
+      await browser.takeScreenshot();
     }
-}
+  },
+  hostname: "localhost",
+  port: 4444,
+  path: "/wd/hub",
+};
